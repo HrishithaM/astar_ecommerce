f1246cacc2fe1eb00656d289c88f7ec67436409e
Pragna-r27 <sreddy@seattleu.edu>
2024-08-09 19:03:03 -0700

    Pragna's new branch

--- client/src/components/About/About.jsx
+++ client/src/components/About/About.jsx
@@ -1,10 +1,18 @@
 
-import React from 'react'
+import React from 'react';
+import Item1 from './Item1';
+import Item2 from './Item2';
+import Item3 from './Item3';
 
-export const About = () => {
-  return (
-    <div>About</div>
-  )
+const About = () => {
+    return (
+      
+        <div className="About">
+            <Item1 />
+            <Item2 />
+            <Item3 />
+        </div>
+    );
 }
 
-export default About
+export default About;
